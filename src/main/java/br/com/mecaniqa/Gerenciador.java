5f0b4e990a83774be50b61a41bf244c645acd596
Vitor Hugo <vitorhugodasilvaoliveira80@gmail.com>
2026-08-06 12:16:26 -0300

    Inserindo servico

--- src/main/java/br/com/mecaniqa/Gerenciador.java
+++ src/main/java/br/com/mecaniqa/Gerenciador.java
@@ -14,22 +14,54 @@ public class Gerenciador {
     public static void main(String[] args) {
 
         inserirPeca(new Peca(1, "parafuso", "Bosch", 2.50, 5.00, 100));
-
-        for (int i = 0; i < totalPecas; i++) {
-            System.out.println("Índice " + i + ": " +
-                    "Código: " + pecas[i].codigo +
-                    " | Nome: " + pecas[i].nomePeca +
-                    " | Preço: R$ " + pecas[i].precoVenda +
-                     " | Quantidade: " + pecas[i].quantidade);
-        }
+        inserirPeca(new Peca(2,"chave estrela","Phillips",3.00,6.50,50));
+        inserirServico(new Servico(1,"manuntenção do motor do carro",65,300.00));
+        //metodos para mostrar o array
+        mostrarPeca();
+        mostrarServico();
     }
     public static void inserirPeca(Peca nome){
         if(totalPecas < MAX_PECAS){
             pecas[totalPecas] = nome;
             totalPecas++;
-        }else{
+        }else {
             System.out.println("Array cheio!");
         }
+    }
+
+    public static void mostrarPeca(){
+        System.out.println("LISTA DE PEÇAS");
+        for (int i = 0; i < totalPecas; i++) {
+            System.out.println(
+                    "Índice " + i + ": " +
+                    "Código: " + pecas[i].codigo +
+                    " | Nome: " + pecas[i].nomePeca +
+                    " | Preço: R$ " + pecas[i].precoVenda +
+                    " | Quantidade: " + pecas[i].quantidade);
+        }
+
+    }
+
+    public static void inserirServico(Servico nome){
+        if(totalServicos < MAX_SERVICOS){
+            servicos[totalServicos] = nome;
+            totalServicos++;
+        }else {
+            System.out.println("Array cheio!");
+        }
+    }
+
+    public static void mostrarServico(){
+        System.out.println("LISTA DE SERVIÇOS");
+        for (int i = 0; i < totalServicos; i++) {
+            System.out.println(
+                    "Índice " + i + ": " +
+                            "Código: " + servicos[i].codigo +
+                            " | Descrição: " + servicos[i].descricao +
+                            " | Tempo: " + servicos[i].tempoMinutos + " minutos " +
+                            " | Valor: R$ " + servicos[i].valor);
+        }
+
     }
 }
 
--- src/main/java/br/com/mecaniqa/Servico.java
+++ src/main/java/br/com/mecaniqa/Servico.java
@@ -3,6 +3,13 @@ package br.com.mecaniqa;
 public class Servico{
     public int codigo;
     public String descricao;
-    public int tempo;
+    public int tempoMinutos;
     public double valor;
+
+    public Servico(int codigo, String descricao, int tempo, double valor){
+        this.codigo = codigo;
+        this.descricao = descricao;
+        this.tempoMinutos = tempo;
+        this.valor = valor;
+    }
 }
