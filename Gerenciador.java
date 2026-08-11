da58ad9f82ebbf4068db68eadd46eacb6a57851d
vitorhugosilvaoliveira <vitorhugodasilvaoliveira80@gmail.com>
2026-08-11 10:06:30 -0300

    Add files via upload
    recolocando a classe gerenciador

---
+++ Gerenciador.java
@@ -0,0 +1,41 @@
+package br.com.mecaniqa;
+
+import static br.com.mecaniqa.Peca.pecas;
+
+public class Gerenciador {
+
+    public static void main(String[] args) {
+
+        Peca pc1= new Peca(1, "parafuso", "Bosch", 2.50, 5.00, 100);
+        Peca pc2= new Peca(2,"chave estrela","Phillips",3.00,6.50,50);
+        Servico serv1 = new Servico(1,"manuntenção do motor do carro",65,300.00);
+
+        Peca.inserirPeca(pc1);
+        Peca.inserirPeca(pc2);
+        Servico.inserirServico(serv1);
+        Servico.mostrarServico();
+        deletarPeca(pc1);
+        Peca.inserirPeca(pc1);
+        Peca.mostrarPeca();
+    }
+    public static void deletarPeca(Peca y) {
+        for (int i = 0; i < pecas.length; i++) {
+            if (pecas[i] == y) {
+                pecas[i] = null;
+                Peca.totalPecas--;
+            }
+
+        }
+        int indexValido = 0;
+        for (int i = 0; i < pecas.length; i++) {
+            if (pecas[i] != null) {
+                pecas[indexValido] = pecas[i];
+                if (i != indexValido) {
+                    pecas[i] = null;
+                }
+                indexValido++;
+            }
+        }
+    }
+}
+
