1b04d5da199829aef7ad251367680cca2eb8334a
Vitor Hugo <vitorhugodasilvaoliveira80@gmail.com>
2026-08-16 19:54:22 -0300

    Inclusão do motor de busca e alteração de outros métodos

--- src/main/java/br/com/mecaniqa/Peca.java
+++ src/main/java/br/com/mecaniqa/Peca.java
@@ -10,7 +10,7 @@ public class Peca {
 
     private static final int MAX_PECAS = 100;
     public static Peca[] pecas = new Peca[MAX_PECAS];
-    public static int totalPecas = 0;
+    public static int indexValido = 0;
 
     public Peca(int codigo, String nomePeca, String nomeFabricante, double precoCusto, double precoVenda, int quantidade) {
         this.codigo = codigo;
@@ -21,28 +21,78 @@ public class Peca {
         this.quantidade = quantidade;
     }
 
-    public static void inserirPeca(Peca nome){
-        if(totalPecas < MAX_PECAS){
-            pecas[totalPecas] = nome;
-            totalPecas++;
-        }else {
-            System.out.println("cheio!");
+    public static void inserirPeca(Peca nome) {
+
+        //verifica se a lista esta cheia
+        if (indexValido <= MAX_PECAS - 1){
+            pecas[indexValido] = nome;
+            indexValido++;
+        }
+        else
+            System.out.println("A lista esta cheia");
+    }
+
+    public static void inserirPosicaoPeca(Peca nome, int index){
+
+        //verifica se o index é válido
+        if (index > indexValido || index < 0){
+            System.out.println("index invalido");
+            return;
+        }
+        //verifica se a lista está cheia
+        if (indexValido >= MAX_PECAS){
+            System.out.println("A lista esta cheia");
+            return;
+        }
+        //insere a peça
+        for (int i = indexValido; i > index; i--){
+            pecas[i] = pecas[i - 1];
+        }
+        pecas[index] = nome;
+        indexValido++;
+    }
+
+
+    public static void mostrarPeca(){
+        System.out.println("LISTA DE PECAS");
+        for (int i = 0; i < indexValido; i++) {
+            System.out.println(
+                    "Indice " + i + ": " +
+                            "Codigo: " + pecas[i].codigo +
+                            " | Nome: " + pecas[i].nomePeca +
+                            " | Preco: R$ " + pecas[i].precoVenda +
+                            " | Quantidade: " + pecas[i].quantidade);
         }
     }
 
-    public static void mostrarPeca(){
-        System.out.println("LISTA DE PEÇAS");
-        for (int i = 0; i < totalPecas; i++) {
-            if (pecas[i] != null){
-                System.out.println(
-                        "Índice " + i + ": " +
-                                "Código: " + pecas[i].codigo +
-                                " | Nome: " + pecas[i].nomePeca +
-                                " | Preço: R$ " + pecas[i].precoVenda +
-                                " | Quantidade: " + pecas[i].quantidade);
-            }
-            else
-                System.out.println("Índice " + i + ": " + " Vazio");
+    public static void deletarPeca(int index){
+
+        //verifica se o index é valido
+        if (index > indexValido || index < 0){
+            System.out.println("index invalido");
+            return;
         }
+
+        //traz os elementos para a esquerda sobrescrevendo a peça do index removido
+        for(int i = index; i < indexValido; i++){
+            pecas[i] = pecas[i + 1];
+        }
+        indexValido--;
+
     }
+
+    public static int buscarPeca(Peca nome){
+        //varre a lista procurando
+        for (int i = 0; i < indexValido; i++){
+            if (pecas[i] == nome)
+                return i;
+        }
+        return -1;
+    }
+
+    //retorna o total de peças com base no index válido naquele momento
+    public static int totalPecas(){
+        return indexValido;
+    }
+
 }
